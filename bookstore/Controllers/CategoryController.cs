4fda7526a7b476c404c986e5fcb00a682fe0f692
Janus Developer <dev@retrievo.net>
2024-01-05 15:14:47 -0400

    add category edit controller

--- bookstore/Controllers/CategoryController.cs
+++ bookstore/Controllers/CategoryController.cs
@@ -65,14 +65,62 @@ namespace bookstore.Controllers
             {
                 _db.Database
                     .ExecuteSql($@"
-                    INSERT INTO dbo.Categories (Name, DisplayOrder)
-                    VALUES ({category.Name}, {category.DisplayOrder});
-                ");
+                        INSERT INTO dbo.Categories (Name, DisplayOrder)
+                        VALUES ({category.Name}, {category.DisplayOrder});
+                    ");
                 return RedirectToAction("Index", "Category"); // redirects to the specified ACTION of secified CONTROLLER
             }
+
             return View();
+        }
 
+        public IActionResult Edit(int? categoryId)
+        {
+            if (categoryId == null || categoryId == 0) return NotFound();
 
+            var category = _db.Categories
+                .FromSql($@"
+                    SELECT * FROM dbo.Categories
+                    WHERE Id = {categoryId}
+                ").FirstOrDefault();
+
+            if (category == null) return NotFound();
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            // Add our own custom checks
+            // Mame and ValidationOrder cannot be same
+            if (!category.Name.IsNullOrEmpty() && category.Name.Equals(category.DisplayOrder.ToString(), StringComparison.CurrentCultureIgnoreCase))
+            {
+                // update ModelState with custome error (key, errormessage) where key 
+                // is the name of the field in the model you wish to display this error for.
+                ModelState.AddModelError("Name", "The Display Order cannot exactly match the Name.");
+            }
+
+            // If name is "test"
+            if (!category.Name.IsNullOrEmpty() && category.Name.Equals("test", StringComparison.CurrentCultureIgnoreCase))
+            {
+                // update ModelState with custome error (key, errormessage) where key 
+                // is the name of the field in the model you wish to display this error for.
+                ModelState.AddModelError("", "Test is an invalid value.");
+            }
+
+            if (ModelState.IsValid)  // checks that inputs are valid according to annotations on the model we are working with
+            {
+                _db.Database
+                    .ExecuteSql($@"
+                        UDATE dbo.Categories SET 
+                        Name = {category.Name}, DisplayOrder = {category.DisplayOrder}
+                        WHERE Id ={category.Id};
+                    ");
+                return RedirectToAction("Index", "Category"); // redirects to the specified ACTION of secified CONTROLLER
+            }
+
+            return View();
         }
     }
 }
