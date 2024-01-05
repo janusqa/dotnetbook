f8ef28b78fae1a3b25e41af8336e4c2e6e4b5842
Janus Developer <dev@retrievo.net>
2024-01-05 11:33:06 -0400

    connect form fields to model with asp-for in html and annotations in the model

--- bookstore/Models/Category.cs
+++ bookstore/Models/Category.cs
@@ -1,15 +1,17 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace bookstore.Models
 {
     public class Category
     {
-        [Key]
+        [Key] // This annotation indicates to EF that this is a Primary Key
         public int Id { get; set; }
 
-        [Required]
+        [Required] // This annotation indicates to EF that this is a required field
         public required string Name { get; set; }
 
+        [DisplayName("Display Order")] // This annotation indicates to EF that this is the text we want to display for this field in the UI when using asp-for
         public int DisplayOrder { get; set; }
     }
 }
